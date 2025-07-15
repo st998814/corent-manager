78b643e3e5deb5aff05910b577dc2e8fbcbb2c7f
st998814 <116810151+st998814@users.noreply.github.com>
2025-07-15 22:40:27 +0930

    pages and route

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -1,2 +1,12 @@
 import React from 'react';
+const Footer: React.FC = () => {
+  return (
+    <div>
+      <p>Footer</p>
+      <p>@All Right Reserved</p>
+    </div>
+  );
+};
 
+
+export default Footer;
