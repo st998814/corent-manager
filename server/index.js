b4670f81562cc9e3e4654ab2fd91868d0487205c
st998814 <116810151+st998814@users.noreply.github.com>
2025-08-05 20:07:34 +0930

    backend server shutdown due to the ip address config , will fix that tmr

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
